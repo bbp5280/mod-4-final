51cab301f738c9793368e8e23fb81c54f9680d7b
Ben Porter <bbp5280@gmail.com>
2017-12-18 13:44:49 -0700

    post new items from for running

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -67,14 +67,30 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-const postNewItem = event => {
+const submitNewItem = event => {
   event.preventDefault();
-  let itemName = $('.item-name').val();
-  let lingerReason = $('.item-linger-reason').val();
-  let condition = $('.dropdown-option').val();
+  let itemToPost = {
+    itemName: $('.item-name').val(),
+    lingerReason: $('.item-linger-reason').val(),
+    cleanliness: $('.dropdown-option').val()
+  };
+
+  console.log(itemToPost);
+  postNewItem(itemToPost);
 };
 
-$('.add-items-form').on('click', '.submit-button', event => inputValues(event));
+const postNewItem = itemObject => {
+
+  fetch('/api/v1/garage_items', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(itemObject)
+  }).then(response => response.json()).then(parsed => console.log(parsed)).catch(error => console.log(error));
+};
+
+$('.add-items-form').on('click', '.submit-button', event => submitNewItem(event));
 
 /***/ })
 /******/ ]);
--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,28 @@
-const postNewItem = (event) => {
+const submitNewItem = (event) => {
   event.preventDefault();
-  let itemName = $('.item-name').val();
-  let lingerReason = $('.item-linger-reason').val();
-  let condition = $('.dropdown-option').val()
+  let itemToPost = {
+    itemName: $('.item-name').val(),
+    lingerReason: $('.item-linger-reason').val(),
+    cleanliness: $('.dropdown-option').val()
+  };
+
+  console.log(itemToPost);
+  postNewItem(itemToPost);
+}
+
+const postNewItem = (itemObject) => {
+
+  fetch('/api/v1/garage_items', {
+    method: 'POST',
+    headers:{
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(itemObject)
+  })
+    .then(response => response.json())
+    .then(parsed => console.log(parsed))
+    .catch(error => console.log(error));
 }
 
 
-
-$('.add-items-form').on('click', '.submit-button', (event) => inputValues(event));
+$('.add-items-form').on('click', '.submit-button', (event) => submitNewItem(event));
