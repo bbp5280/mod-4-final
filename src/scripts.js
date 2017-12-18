569b28127d5339dfadfed8ecbea9fa43c6f8b181
Ben Porter <bbp5280@gmail.com>
2017-12-18 14:19:28 -0700

    add items to dom count number of items

--- src/scripts.js
+++ src/scripts.js
@@ -11,7 +11,6 @@ const submitNewItem = (event) => {
 }
 
 const postNewItem = (itemObject) => {
-
   fetch('/api/v1/garage_items', {
     method: 'POST',
     headers:{
@@ -24,5 +23,32 @@ const postNewItem = (itemObject) => {
     .catch(error => console.log(error));
 }
 
+const getItems = () => {
+  fetch('/api/v1/garage_items')
+    .then(response => response.json())
+    .then(parsed => appendGarageItems(parsed))
+    .catch(error => console.log(error))
+}
+
+const appendGarageItems = (items) => {
+  items.forEach(item => {
+    $('.items-list').append(
+      `<li class='garage-item item-id-${item.id}'>${item.itemName}</li>`);
+  })
+  numberOfGarageItems(items);
+}
+
+const numberOfGarageItems = (items) => {
+  $('.items-count-container').append(
+    `<p class='item-count'>You Have ${items.length} In Your Garage</p>`
+  );
+}
+
+const filterCondition = (items, condition) {
+  const filteredItems = items.filter(item => item.cleanliness === condition)
+  
+}
+
 
 $('.add-items-form').on('click', '.submit-button', (event) => submitNewItem(event));
+$(document).ready(() => getItems())
