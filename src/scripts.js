8f956cf9e0e617eeb506d0971700f7888683441d
Ben Porter <bbp5280@gmail.com>
2017-12-18 16:28:38 -0700

    toggle details of the garage items

--- src/scripts.js
+++ src/scripts.js
@@ -3,9 +3,11 @@ const submitNewItem = (event) => {
   let itemToPost = {
     itemName: $('.item-name').val(),
     lingerReason: $('.item-linger-reason').val(),
-    cleanliness: $('.dropdown-option').val()
+    cleanliness: $('.condition-select').val()
   };
   postNewItem(itemToPost);
+  $('.item-name').val('')
+  $('.item-linger-reason').val('')
 }
 
 const postNewItem = (itemObject) => {
@@ -17,28 +19,39 @@ const postNewItem = (itemObject) => {
     body: JSON.stringify(itemObject)
   })
     .then(response => response.json())
-    .then(parsed => console.log(parsed))
+    .then(parsed => getItems())
     .catch(error => console.log(error));
 }
 
-const appendNewItem = () => {
-  
-}
-
 const getItems = () => {
+  $('.items-list').html('')
+  $('.items-count-container').html('')
   fetch('/api/v1/garage_items')
     .then(response => response.json())
     .then(parsed => appendGarageItems(parsed))
     .catch(error => console.log(error))
 }
 
+getItems()
+
 const appendGarageItems = (items) => {
   items.forEach(item => {
     $('.items-list').append(
-      `<li class='garage-item item-id-${item.id}'>${item.itemName}</li>`);
+      `<h3 class='garage-item-name item-id-${item.id}' id='item${item.id}'>${item.itemName}</h3>
+       <div class='garage-item-details item-id-${item.id} inactive-details'>
+       <p class='item-details item-id-${item.id}'>Reason For Holding: ${item.lingerReason}</p>
+       <p class='item-details item-id-${item.id}'>Cleanliness Level: ${item.cleanliness}</p>
+     </div>`);
   })
   numberOfGarageItems(items);
   numberOfItemCleanliness(items);
+}
+
+const showDetails = (event) => {
+  const itemNumber = event.target.id.slice(4)
+  const divToShow = $(`.item-id-${itemNumber}`)
+
+  divToShow.toggleClass('active-details')
 }
 
 const numberOfGarageItems = (items) => {
@@ -62,6 +75,5 @@ const numberOfItemCleanliness = (items) => {
 }
 
 
-
 $('.add-items-form').on('click', '.submit-button', (event) => submitNewItem(event));
-$(document).ready(() => getItems())
+$('.items-container').on('click', '.garage-item-name', (event) => showDetails(event));
